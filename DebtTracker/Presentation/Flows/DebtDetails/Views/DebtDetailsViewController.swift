133fc01d5e6555ef0913073ed342a92027f6bd50
Kirill Somov <gornomirov661@gmail.com>
2025-03-26 15:02:28 +0300

    done general info

--- DebtTracker/Presentation/Flows/DebtDetails/Views/DebtDetailsViewController.swift
+++ DebtTracker/Presentation/Flows/DebtDetails/Views/DebtDetailsViewController.swift
@@ -4,9 +4,23 @@ import UIKit
 class DebtDetailsViewController: UIViewController {
     // MARK: Lifecycle
 
+    let generalDebtInfo: GeneralDebtInfo = .init()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        setupUI()
+        setupConstraints()
+    }
 
-        view.backgroundColor = .red
+    private func setupUI() {
+        view.addSubview(generalDebtInfo)
+    }
+
+    private func setupConstraints() {
+        generalDebtInfo.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
     }
 }
---
+++ DebtTracker/Presentation/Flows/DebtDetails/Views/GeneralDebtInfo.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+class GeneralDebtInfo: UIView {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Сумма кредита"
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .white
+        return label
+    }()
+
+    private let amountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2500000.00 ₽"
+        label.font = .systemFont(ofSize: 32, weight: .semibold)
+        label.textColor = .white
+        return label
+    }()
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    private func setupView() {
+        backgroundColor = UIColor(named: "BlackCustomColor")
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+
+        addSubview(titleLabel)
+        addSubview(amountLabel)
+
+        titleLabel.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview().inset(16)
+        }
+
+        amountLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom)
+            make.bottom.leading.trailing.equalToSuperview().inset(16)
+        }
+    }
+}

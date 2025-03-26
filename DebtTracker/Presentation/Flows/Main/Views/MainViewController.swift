dabb8105c28c1939bd362beb5627212e2d9ace9f
BogdanTarchenko <tarchenko.b>
2025-03-26 16:00:06 +0300

    Fix linter errors

--- DebtTracker/Presentation/Flows/Main/Views/MainViewController.swift
+++ DebtTracker/Presentation/Flows/Main/Views/MainViewController.swift
@@ -6,14 +6,26 @@ class MainViewController: UITabBarController {
     weak var coordinator: MainCoordinator?
     private let factory: MainViewControllerFactory
 
-    init(factory: MainViewControllerFactory) {
+    init(
+        factory: MainViewControllerFactory
+    ) {
         self.factory = factory
-        super.init(nibName: nil, bundle: nil)
+        super.init(
+            nibName: nil,
+            bundle: nil
+        )
     }
 
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    @available(
+        *,
+        unavailable
+    )
+    required init?(
+        coder: NSCoder
+    ) {
+        fatalError(
+            "init(coder:) has not been implemented"
+        )
     }
 
     override func viewDidLoad() {
@@ -26,15 +38,26 @@ class MainViewController: UITabBarController {
 private extension MainViewController {
     func setupUI() {
         let appearance = UITabBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = UIColor(named: "TabBarColor")
-        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(named: "TabBarItemActiveColor")
-        appearance.stackedLayoutAppearance.normal.iconColor = UIColor(named: "TabBarItemInactiveColor")
+        appearance
+            .configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(
+            named: "TabBarColor"
+        )
+        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(
+            named: "TabBarItemActiveColor"
+        )
+        appearance.stackedLayoutAppearance.normal.iconColor = UIColor(
+            named: "TabBarItemInactiveColor"
+        )
         appearance.stackedLayoutAppearance.selected.titleTextAttributes = [
-            .foregroundColor: UIColor(named: "TabBarItemActiveColor")
+            .foregroundColor: UIColor(
+                named: "TabBarItemActiveColor"
+            )
         ]
         appearance.stackedLayoutAppearance.normal.titleTextAttributes = [
-            .foregroundColor: UIColor(named: "TabBarItemInactiveColor")
+            .foregroundColor: UIColor(
+                named: "TabBarItemInactiveColor"
+            )
         ]
 
         tabBar.standardAppearance = appearance
@@ -48,21 +71,31 @@ private extension MainViewController {
 
         homeViewController1.tabBarItem = UITabBarItem(
             title: "Главная",
-            image: UIImage(systemName: "house.fill"),
+            image: UIImage(
+                systemName: "house.fill"
+            ),
             tag: 0
         )
         homeViewController2.tabBarItem = UITabBarItem(
             title: "Главная",
-            image: UIImage(systemName: "house.fill"),
+            image: UIImage(
+                systemName: "house.fill"
+            ),
             tag: 1
         )
         homeViewController3.tabBarItem = UITabBarItem(
             title: "Главная",
-            image: UIImage(systemName: "house.fill"),
+            image: UIImage(
+                systemName: "house.fill"
+            ),
             tag: 2
         )
 
-        let viewControllers = [homeViewController1, homeViewController2, homeViewController3]
+        let viewControllers = [
+            homeViewController1,
+            homeViewController2,
+            homeViewController3
+        ]
         self.viewControllers = viewControllers
     }
 }

b063c7b86f3dd48804a1fc7581d07fb3f9aca3a1
oooooorg <oooooorg@yandex.ru>
2025-03-27 17:06:05 +0300

    fixes

--- DebtTracker/Presentation/Flows/Calculator/Components/InputField.swift
+++ DebtTracker/Presentation/Flows/Calculator/Components/InputField.swift
@@ -2,16 +2,24 @@ import SwiftUI
 
 struct InputField: View {
     var title: String
-    @Binding var text: String
+    @Binding var value: Double
     var keyboardType: UIKeyboardType = .decimalPad
+
+    @State private var textValue: String = ""
 
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             Text(title)
                 .font(.headline)
-            TextField("Enter \(title.lowercased())", text: $text)
+            TextField("Enter \(title.lowercased())", text: $textValue)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .keyboardType(keyboardType)
+                .onChange(of: textValue) { _, newValue in
+                    value = Double(newValue) ?? value
+                }
+                .onAppear {
+                    textValue = String(value)
+                }
         }
         .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
     }
